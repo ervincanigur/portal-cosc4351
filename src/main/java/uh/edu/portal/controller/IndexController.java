4ce5b7228e603dbef00501f01f5ff3b4e6b33790
xcode <ervin@canigur.com>
2020-11-29 00:16:13 -0600

    login is working

--- src/main/java/uh/edu/portal/controller/IndexController.java
+++ src/main/java/uh/edu/portal/controller/IndexController.java
@@ -3,14 +3,13 @@ package uh.edu.portal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-	public String greeting(Model model) {
+    public String greeting(Model model) {
         model.addAttribute("errorMessage", "");
-        return "login"; // what mapping we will (str format)
-	}
+        return "login";
+    }
 }
--- src/main/java/uh/edu/portal/controller/WelcomeController.java
+++ src/main/java/uh/edu/portal/controller/WelcomeController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/welcome")
-	public String greeting(@RequestParam(name="username", required=true) String username, Model model) {
+    public String greeting(@RequestParam(name = "username", required = true) String username, Model model) {
         model.addAttribute("username", username);
-		return "welcome"; // what mapping we will (str format)
-	}
+        return "welcome";
+    }
 }

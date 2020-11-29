3538030cab2e3a143d235d8b424003245678bee5
Drew Williams <61474426+DDub97@users.noreply.github.com>
2020-11-29 00:18:34 -0600

    Welcome

--- src/main/java/uh/edu/portal/controller/WelcomeController.java
+++ src/main/java/uh/edu/portal/controller/WelcomeController.java
@@ -10,8 +10,49 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-	public String greeting(@RequestParam(name="username", required=true) String username, Model model) {
-        model.addAttribute("username", username);
+	public String greeting(@RequestParam(name="username", required=true) String username, Model model, String link1, String link2, String link3, String link4, String link5) {
+		if(username == "Support" || username == "support"){
+			link1 = "Manage User Accounts";
+			link2 = "Assign Roles";
+			link3 = "Help Desk";
+			link4 = "";
+			link5 = "";
+		}
+		else if(username == "Finance" || username == "finance"){
+			link1 = "Finance Reports";
+			link2 = "Accounts Payable";
+			link3 = "Accounts Recievable";
+			link4 = "";
+			link5 = "";
+		}
+		else if(username == "Sales" || username == "sales"){
+			link1 = "Sales";
+			link2 = "Sales Leads";
+			link3 = "Sales Demo";
+			link4 = "";
+			link5 = "";
+		}
+		else if(username == "HR" || username == "hr" || username == "Hr"){
+			link1 = "New Hire On-boarding";
+			link2 = "Benefits";
+			link3 = "Payroll";
+			link4 = "Off-boarding";
+			link5 = "HR Reports";
+		}
+		else if(username == "Technology" || username == "technology"){
+			link1 = "Application Monitoring";
+			link2 = "Tech Support";
+			link3 = "App Development";
+			link4 = "App Admin";
+			link5 = "Release Management";
+		}
+		
+		model.addAttribute("username", username);
+		model.addAttribute("link1", link1);
+		model.addAttribute("link2", link2);
+		model.addAttribute("link3", link3);
+		model.addAttribute("link4", link4);
+		model.addAttribute("link5", link5);
 		return "welcome"; // what mapping we will (str format)
 	}
 }

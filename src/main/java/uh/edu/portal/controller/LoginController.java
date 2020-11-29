a66880d595e8768b067f0c6c76e1c93f3f32a289
xcode <ervin@canigur.com>
2020-11-28 22:55:26 -0600

    stuff and things

---
+++ src/main/java/uh/edu/portal/controller/LoginController.java
@@ -0,0 +1,15 @@
+package uh.edu.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		return "login"; // what mapping we will (str format)
+	}
+}
